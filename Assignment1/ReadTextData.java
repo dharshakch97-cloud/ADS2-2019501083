30b0a51889e03617a82b892724a318d5455512e4
dharshakch97-cloud <dharshak.ch97@msitprogram.net>
2020-01-07 16:38:30 +0530

    Done

--- Assignment1/ReadTextData.java
+++ Assignment1/ReadTextData.java
@@ -1,10 +1,18 @@
-  
+/**
+ * @author Dharshak
+ * 
+ * Assignment 1 Reading text data(CSV files)
+ */  
 import java.io.*;
 import java.util.*;
 
 class ReadTextData {
+
+    /**
+     * Main method for reading text data
+     */
     public static void main(String[] args) throws FileNotFoundException, IOException {
-        ReadTextData r = new ReadTextData();
+        ReadTextData r = new ReadTextData(); // creating object for class ReadTextData
         String synsets_file = "synsets.txt";
         String hypernyms_file = "hypernyms.txt";
         r.parseSynsets(synsets_file);
@@ -12,9 +20,15 @@ class ReadTextData {
         r.parseHypernyms(hypernyms_file);
     } 
 
+    /**
+     * parseSynsets method to read the synsets.txt file and
+     * displaying in the console (or terminal).
+     * 
+     * @param filename string that contains filename with extension
+     */
     private void parseSynsets(String filename) throws FileNotFoundException, IOException {
-        File  read_synsets = new File(filename);
-        BufferedReader br = new BufferedReader(new FileReader(read_synsets)); 
+        File  read_synsets = new File(filename); // loads the file
+        BufferedReader br = new BufferedReader(new FileReader(read_synsets)); // reads the file
         String st;
         int i = 0;
         while ((st = br.readLine()) != null) {
@@ -26,9 +40,15 @@ class ReadTextData {
         } 
     }
 
+    /**
+     * parseSynsets method to read the hypernyms.txt file and
+     * displaying in the console (or terminal).
+     * 
+     * @param filename string that contains filename with extension
+     */
     private void parseHypernyms(String filename) throws FileNotFoundException, IOException {
-        File  read_hypernyms = new File(filename);
-        BufferedReader br = new BufferedReader(new FileReader(read_hypernyms)); 
+        File  read_hypernyms = new File(filename); // loads the file 
+        BufferedReader br = new BufferedReader(new FileReader(read_hypernyms)); // reads the file
         String st;
         int i = 0;
         while ((st = br.readLine()) != null) {
