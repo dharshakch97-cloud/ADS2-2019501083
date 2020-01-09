4c7024c86dbc7f39a338ca4d7f93d2bdc2a31c7b
dharshakch97-cloud <dharshak.ch97@msitprogram.net>
2020-01-09 16:57:08 +0530

    Done

--- Assignment3/SAP.java
+++ Assignment3/SAP.java
@@ -7,20 +7,20 @@ public class SAP {
     Digraph digraph;
     // constructor takes a digraph (not necessarily a DAG)
     public SAP(Digraph G) {
-        if (G == null) {
-            throw IllegalArgumentException("Graph is null");
-        }
+        // if (G == null) {
+        //     throw IllegalArgumentException("Graph is null");
+        // }
         digraph = new Digraph(G);
     }
  
-    private void validateVertex(int v) {
+    private void validVertex(int v) {
         int V = digraph.V();
         if (v < 0 && v >= V) {
             throw new IndexOutOfBoundsException("vertex " + v + " is not between 0 and " + (V-1));
         }
     }
  
-    private void validateVertices(Iterable<Integer> vertices) {
+    private void validVertices(Iterable<Integer> vertices) {
         if (vertices == null) {
             throw new NullPointerException("argument is null");
         }
@@ -34,64 +34,64 @@ public class SAP {
 
     // return shortest length and corresponding ancestor
     private int[] shortest(int v, int w) {
-        validateVertex(v);
-        validateVertex(w);
-        int[] result = new int[2];
+        validVertex(v);
+        validVertex(w);
+        int[] res = new int[2];
         BreadthFirstDirectedPaths bfsv = new BreadthFirstDirectedPaths(digraph, v);
         BreadthFirstDirectedPaths bfsw = new BreadthFirstDirectedPaths(digraph, w);
         
-        int shortestLen = Integer.MAX_VALUE;
-        int shortestAncestor = -1;
+        int shortest_Len = Integer.MAX_VALUE;
+        int shortest_Ancestor = -1;
         for (int i = 0; i < digraph.V(); ++i) {
             if (bfsv.hasPathTo(i) && bfsw.hasPathTo(i)) {
                 int len = bfsv.distTo(i) + bfsw.distTo(i);
-                if (len < shortestLen) {
-                    shortestLen = len;
-                    shortestAncestor = i;
+                if (len < shortest_Len) {
+                    shortest_Len = len;
+                    shortest_Ancestor = i;
                 }
             }
         }
         
-        if (shortestAncestor == -1) {
-            result[0] = -1;
-            result[1] = -1;
+        if (shortest_Ancestor == -1) {
+            res[0] = -1;
+            res[1] = -1;
         }
         else {
-            result[0] = shortestLen;
-            result[1] = shortestAncestor;
+            res[0] = shortest_Len;
+            res[1] = shortest_Ancestor;
         }
-        return result;
+        return res;
     }
 
     // return shortest length and corresponding ancestor
     private int[] shortest(Iterable<Integer> v, Iterable<Integer> w) {
-        validateVertices(v);
-        validateVertices(w);
-        int[] result = new int[2];
+        validVertices(v);
+        validVertices(w);
+        int[] res = new int[2];
         BreadthFirstDirectedPaths bfsv = new BreadthFirstDirectedPaths(digraph, v);
         BreadthFirstDirectedPaths bfsw = new BreadthFirstDirectedPaths(digraph, w);
         
-        int shortestLen = Integer.MAX_VALUE;
-        int shortestAncestor = -1;
+        int shortest_Len = Integer.MAX_VALUE;
+        int shortest_Ancestor = -1;
         for (int i = 0; i < digraph.V(); ++i) {
             if (bfsv.hasPathTo(i) && bfsw.hasPathTo(i)) {
                 int len = bfsv.distTo(i) + bfsw.distTo(i);
-                if (len < shortestLen) {
-                    shortestLen = len;
-                    shortestAncestor = i;
+                if (len < shortest_Len) {
+                    shortest_Len = len;
+                    shortest_Ancestor = i;
                 }
             }
         }
         
-        if (shortestAncestor == -1) {
-            result[0] = -1;
-            result[1] = -1;
+        if (shortest_Ancestor == -1) {
+            res[0] = -1;
+            res[1] = -1;
         }
         else {
-            result[0] = shortestLen;
-            result[1] = shortestAncestor;
+            res[0] = shortest_Len;
+            res[1] = shortest_Ancestor;
         }
-        return result;
+        return res;
     }
 
     // length of shortest ancestral path between v and w; -1 if no such path
